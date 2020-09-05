acdba2baa273d4256450f782e676984b985c34bb
yashmunjal <yashmu45@gmail.com>
2020-09-06 02:42:48 +0530

    alert-done

--- Readme.md
+++ Readme.md
@@ -9,11 +9,11 @@ For Live demo visit https://yashkameet.herokuapp.com
 - [x] Homepage UI
 - [x] Rooms video cards made responsive
 - [x] Hiding/showing Audio and video
-- [ ]  Showing Room id as an alert for new users
+- [x]  Showing Room id as an alert for new users
 - [x] Correctinhg the buttons UI
 - [ ] Accepting names and creating a session for a specified user
 
-#### Long term Plan
+- ### Long term Plans
 
-[ ] Screen sharing option
-[ ] Increasing server optimizations
+- [ ] Screen sharing option
+- [ ] Increasing server optimizations
--- public/script.js
+++ public/script.js
@@ -129,3 +129,23 @@ function audioController() {
   audio_flags++;
   myStream.getTracks()[0].enabled = !myStream.getTracks()[0].enabled;
 }
+
+
+//modal
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+
+window.onload = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
